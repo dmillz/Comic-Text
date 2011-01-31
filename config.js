e2494361491210481d36a9bb3be185b2d33f2000
David Mills <github@millz.com>
2011-01-30 20:27:18 -0500

    CSS tweaks to more closely mimic Chrome

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = config || {};
 config.defaultWhitelist = "xkcd.com";
 
 // css
-config.currentCssVersion = 2;
+config.currentCssVersion = 3;
 
 config.cssVersions = [ 
 
@@ -64,6 +64,34 @@ div.comic-text-popup {\n\
 	-webkit-border-radius: 4px;\n\
 	border: 1px solid #777;\n\
 	color: #666;\n\
+	text-align: left;\n\
+	text-decoration: none;\n\
+	line-height: normal;\n\
+	letter-spacing: normal;\n\
+	text-shadow: none;\n\
+	text-transform: none;\n\
+	list-style: none;\n\
+	text-indent: 0;\n\
+	z-index: 99999999;\n\
+}",
+
+// version 3
+"/* Mimic Chrome as closely as possible */\n\
+div.comic-text-popup {\n\
+	display: none;\n\
+	position: absolute;\n\
+	max-width: 300px;\n\
+	background: -webkit-gradient(linear, left top, left bottom, from(#FFFFFF), to(#e4e5f0));\n\
+	padding: 2px 4px;\n\
+	font-size: 12px;\n\
+	font-family: 'Segoe UI', Calibri, Helvetica, Verdana, sans-serif;\n\
+	font-weight: normal;\n\
+	font-variant: normal;\n\
+	cursor: default;\n\
+	-webkit-border-radius: 4px;\n\
+	-webkit-box-shadow: 1px 1px 2px #888;\n\
+	border: 1px solid #767676;\n\
+	color: #454545;\n\
 	text-align: left;\n\
 	text-decoration: none;\n\
 	line-height: normal;\n\
