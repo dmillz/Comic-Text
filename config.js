beccc513beacc4fd84d7fa8c67ef28122b22cbfa
David Mills <github@millz.com>
2011-01-31 23:12:14 -0500

    yet another css tweak

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = config || {};
 config.defaultWhitelist = "xkcd.com";
 
 // css
-config.currentCssVersion = 3;
+config.currentCssVersion = 4;
 
 config.cssVersions = [ 
 
@@ -100,5 +100,35 @@ div.comic-text-popup {\n\
 	text-transform: none;\n\
 	list-style: none;\n\
 	text-indent: 0;\n\
+	z-index: 99999999;\n\
+}",
+
+// version 4
+"/* Mimic Chrome as closely as possible */\n\
+div.comic-text-popup {\n\
+	max-width: 300px;\n\
+	background: -webkit-gradient(linear, left top, left bottom, from(#FFFFFF), to(#e4e5f0));\n\
+	padding: 2px 4px;\n\
+	font-size: 12px;\n\
+	font-family: 'Segoe UI', Calibri, Helvetica, Verdana, sans-serif;\n\
+	font-weight: normal;\n\
+	font-variant: normal;\n\
+	cursor: default;\n\
+	-webkit-border-radius: 4px;\n\
+	-webkit-box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.65);\n\
+	border: 1px solid #767676;\n\
+	color: #454545;\n\
+	text-align: left;\n\
+	text-decoration: none;\n\
+	line-height: normal;\n\
+	letter-spacing: normal;\n\
+	text-shadow: none;\n\
+	text-transform: none;\n\
+	list-style: none;\n\
+	text-indent: 0;\n\
+	\n\
+	/* don't mess with these */\n\
+	display: none;\n\
+	position: absolute;\n\
 	z-index: 99999999;\n\
 }" ];
